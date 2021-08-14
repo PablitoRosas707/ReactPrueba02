77562a1c4376de0054cab6516a5778c573aa9df8
Pablito <2019c00180p@iedep.edu.mx>
2021-08-13 23:07:00 -0500

    main agregado

---
+++ main/precache-manifest.27ded92942bef9cf1769608dc94ba421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2be7313606e42467459ca9a703bd24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42d37353fbcbd968fe33",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d37353fbcbd968fe33",
+    "url": "/static/js/main.55042db9.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
